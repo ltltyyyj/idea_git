10607382b6d3a115b942828331eaf40e6c153821
123 <dfasdfsadfdsafsa@qq.com>
2018-12-06 21:04:38 +0800

    初始化

---
+++ git_day01_project/src/main/java/com/itheima/domain/User.java
@@ -0,0 +1,12 @@
+package com.itheima.domain;
+
+/**
+ * @ClassName User
+ * @Description
+ * @Author yangjian
+ * @Date 2018/12/6 20:59
+ * @Version 1.0
+ **/
+public class User {
+    private Integer id;
+} 
